781fffd63eb684e7bca364b63af211d9bccad0ac
amin diamond <amindaimond1@gmail.com>
2024-10-24 16:57:34 +0330

    change blog/urls and blog/views

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from blog.views import post_list , category_list, post_detail
 
-urlpatterns = [path('list/', post_list),
-               path('category/list/', category_list),
-               path('detail/<str:post_title>/', post_detail),
-               path('archieve/<int:year>/', post_list)]
+urlpatterns = [
+                path('list/', post_list),
+                path('category/list/', category_list),
+                path('detail/<str:post_title>/', post_detail),
+                path('archieve/<int:year>/', post_list)
+               ]
+
+
 
 
 
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,13 @@ from django.http import HttpResponse
 # from django.shortcuts import render
 
 # Create your views here.
-def post_list(request, year):
-    return HttpResponse("posts lists page")
+def post_list(request, year=None, month=None):
+    if month is not None:
+        return HttpResponse(f"post list archive for {year} and {month}")
+    if year is not None:
+        return HttpResponse(f"post list archive for {year}")
+    
+    return HttpResponse("posts list page")
 
 
 
